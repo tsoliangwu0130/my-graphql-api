5bbade7e563a978c4b2d2ac0927cd0700ce953fe
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-04-04 16:15:29 -0700

    Pass arguments

--- server.js
+++ server.js
@@ -4,22 +4,17 @@ const { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
   type Query {
-    quoteOfTheDay: String,
-    random: Float!
-    rollThreeDice: [Int]
+    rollDice(numDice: Int! numSides: Int): [Int]
   }
 `);
 
 var root = {
-  quoteOfTheDay: () => {
-    var randomNum = Math.random();
-    return randomNum < 0.5 ? `${randomNum} < 0.5` : `${randomNum} > 0.5`;
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6))
+  rollDice: function({ numDice, numSides }) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
   }
 };
 
