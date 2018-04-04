e9c47851a5953085a632a60b7d4d265e359f8b03
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-04-04 14:20:18 -0700

    Run GraphQL API as a server app

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-var { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
   type Query {
-    hello: String
+    hello: String,
+    name: String
   }
 `);
 
 var root = {
   hello: () => {
     return 'Hello GraphQL!';
+  },
+  name: () => {
+    return 'Leon';
   }
 };
 
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
